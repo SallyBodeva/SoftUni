3e715233716cf1415615c68df52d913365121f01
salihe <salihe.bodeva10@gmail.com>
2024-06-10 23:47:41 +0300

    Exercise

--- 01-Programming Basics JavaScript - 2024/002-Conditional_Statement_Exercise/P03-Time+15Minutes.js
+++ 01-Programming Basics JavaScript - 2024/002-Conditional_Statement_Exercise/P03-Time+15Minutes.js
@@ -1,3 +1,23 @@
-function name(params) {
+function AddTime(input) {
+  
+    let hour = Number(input[0]);
+    let minutes = Number(input[1]);
+
+    minutes += 15;
+
+    if(minutes>=60){
+        minutes = minutes - 60;
+        hour++;
+    }
+    if(hour === 24){
+        hour -= 24;
+    }
     
+   if(minutes<10){
+        console.log(`${hour}:0${minutes}`);
+    } else{
+        console.log(`${hour}:${minutes}`);
+    }
 }
+
+AddTime(["23", "59"]);
---
+++ 01-Programming Basics JavaScript - 2024/002-Conditional_Statement_Exercise/P04-ToyShop.js
@@ -0,0 +1,31 @@
+function Workikg(input) {
+    const PUZZLE_PRICE = 2.60;
+    const DOLL_PRICE = 3;
+    const TEDDY_BEAR_PRICE = 4.1;
+    const MINION_PRICE = 8.20;
+    const CAR_PRICE = 2;
+    
+    
+    const tripPrice = Number(input[0]);
+    const puzzelsCount = Number(input[1]);
+    const dollCount = Number(input[2]);
+    const teddyBearsCount = Number(input[3]);
+    const minionsCount  = Number(input[4]);
+    const carsCount = Number(input[5]);
+
+    let totalToysCount = puzzelsCount + teddyBearsCount + dollCount + minionsCount + carsCount;
+    let sum = puzzelsCount * PUZZLE_PRICE + dollCount * DOLL_PRICE + teddyBearsCount * TEDDY_BEAR_PRICE + minionsCount * MINION_PRICE + carsCount * CAR_PRICE;
+    if(totalToysCount >= 50){
+        sum *= 0.75;
+    }
+    sum *= 0.90;
+
+    if(sum >= tripPrice){
+        console.log(`Yes! ${sum - tripPrice} lv left.`);
+    } else{
+        console.log(`Not enough money! ${tripPrice - sum} lv needed.`);
+    }
+}
+
+Workikg(["320","8","2","5","5","1"]);    
+    
