e8ef1fdd9f8eb5646fbad4888dce5057a587eccc
salihe <salihe.bodeva10@gmail.com>
2024-06-15 22:36:39 +0300

    Exercise

--- 01-Programming Basics JavaScript - 2024/002-Conditional_Statements_More_Exercises/P01-PipesInPool.js
+++ 01-Programming Basics JavaScript - 2024/002-Conditional_Statements_More_Exercises/P01-PipesInPool.js
@@ -1,3 +1,17 @@
-function name(params) {
-    
+function fillThePool(input) {
+    const volume = Number(input[0]);
+    const pipe1 = Number(input[1]);
+    const pipe2 = Number(input[2]);
+    const hours = Number(input[3]);
+
+    let pipe1Filling = pipe1 * hours;
+    let pipe2Filling = pipe2 * hours;
+    let totalFilling = pipe1Filling + pipe2Filling;
+
+    if(totalFilling <= volume){
+        console.log(`The pool is ${((totalFilling / volume) * 100).toFixed(2)}% full. Pipe 1: ${((pipe1Filling / totalFilling) * 100).toFixed(2)}%. Pipe 2: ${((pipe2Filling / totalFilling) * 100).toFixed(2)}%."`);
+    } else{
+        console.log(`For ${hours} hours the pool overflows with ${(totalFilling - volume).toFixed(2)} liters."`);
+    }
 }
+fillThePool(["1000","100","120","3"]);
---
+++ 01-Programming Basics JavaScript - 2024/002-Conditional_Statements_More_Exercises/P02-SleepyTomCat.js
@@ -0,0 +1,17 @@
+function sleepingTom(input) {
+    const PLAYS_PER_WORKING_DAY = 63;
+    const PLAYS_PER_HOLIDAY = 127;
+
+    const daysOff = Number(input[0]);
+
+    let totalMinsInPlays = (daysOff * PLAYS_PER_HOLIDAY) + (365 - daysOff) * PLAYS_PER_WORKING_DAY;
+    if(totalMinsInPlays > 30000){
+        console.log(`Tom will run away`);
+        console.log(`${Math.floor((totalMinsInPlays - 30000) / 60)} hours and ${(totalMinsInPlays - 30000) % 60} minutes more for play`);
+    } else{
+        console.log(`Tom sleeps well`);
+        console.log(`${Math.floor((30000 - totalMinsInPlays) / 60)} hours and ${(30000 - totalMinsInPlays) % 60} minutes less for play`);
+    }
+}
+
+sleepingTom(["20"]);
