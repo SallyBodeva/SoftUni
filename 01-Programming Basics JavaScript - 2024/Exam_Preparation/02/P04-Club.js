8dc9dc28fbee0cde3f37f87c0e2270b6a13ef9cc
salihe <salihe.bodeva10@gmail.com>
2024-08-04 23:51:01 +0300

    JS

---
+++ 01-Programming Basics JavaScript - 2024/Exam_Preparation/02/P04-Club.js
@@ -0,0 +1,34 @@
+function profit(input){
+    const targetProfit = Number(input.shift());
+    let currentProfit = 0;
+    while(true){
+        let command = input.shift();
+        if(command === "Party!"){
+            if(currentProfit >= targetProfit){
+                console.log(`Target acquired.`);
+            }else{
+                console.log(`We need ${(targetProfit-currentProfit).toFixed(2)} leva more.`);
+            }
+            break;
+        }
+        let coctailPrice = command.length;
+        let bill = coctailPrice * Number(input.shift());
+        if(bill % 2 == 1){
+            bill *= 0.75;
+        }
+        currentProfit += bill;
+        if(currentProfit >= targetProfit){
+            console.log(`Target acquired.`);
+            break;
+        }
+    }
+    console.log(`Club income - ${currentProfit.toFixed(2)} leva.`);
+}
+
+profit(["100",
+    "Sidecar",
+    "7",
+    "Mojito",
+    "5",
+    "White Russian",
+    "10"]);    
